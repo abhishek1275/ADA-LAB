7b2846051f366fdd8998b6087509ac99e24f53d0
abhishek1275 <53854914+abhishek1275@users.noreply.github.com>
2019-09-12 16:03:17 +0530

    Add files via upload

--- ruf.cpp
+++ ruf.cpp
@@ -1,63 +1,67 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-#define ROW 5 
-#define COL 5 
 
-int isSafe(int M[][COL], int row, int col, 
-		bool visited[][COL]) 
-{ 
+#include <bits/stdc++.h>
+using namespace std;
 
-	return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL) && (M[row][col] && !visited[row][col]); 
-} 
-void DFS(int M[][COL], int row, int col, 
-		bool visited[][COL]) 
-{ 
-
-	static int rowNbr[] = { -1, -1, -1, 0, 0, 1, 1, 1 }; 
-	static int colNbr[] = { -1, 0, 1, -1, 1, -1, 0, 1 }; 
-	visited[row][col] = true; 
+#define ROW 4
+#define COL 4
 
 
-	for (int k = 0; k < 8; ++k) 
-		if (isSafe(M, row + rowNbr[k], col + colNbr[k], visited)) 
-			DFS(M, row + rowNbr[k], col + colNbr[k], visited); 
-} 
+int isSafe(int M[][COL], int row, int col,
+           bool visited[][COL])
+{
+
+    return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL) && (M[row][col] && !visited[row][col]);
+}
 
 
-int countIslands(int M[][COL]) 
-{ 
- 
-	bool visited[ROW][COL]; 
-	memset(visited, 0, sizeof(visited)); 
+void DFS(int M[][COL], int row, int col,
+         bool visited[][COL])
+{
+    static int rowNbr[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    static int colNbr[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    visited[row][col] = true;
 
-	int count = 0; 
-	for (int i = 0; i < ROW; ++i) 
-		for (int j = 0; j < COL; ++j) 
 
-			
-			if (M[i][j] && !visited[i][j]) { 
-				DFS(M, i, j, visited); 
-				++count; 
-			} 
+    for (int k = 0; k < 8; ++k)
+        if (isSafe(M, row + rowNbr[k], col + colNbr[k], visited))
+            DFS(M, row + rowNbr[k], col + colNbr[k], visited);
+}
 
-	return count; 
-} 
 
-int main() 
-{ 
-int n,M[ROW][COL];
-	/*int M[][COL] = { { 1, 1, 0, 0, 0 }, 
-			 { 1, 1, 0, 1, 1 }, 
-			 { 1, 0, 0, 1, 1 }, 
-			 { 0, 0, 0, 0, 0 }, 
-			 { 1, 0, 1, 1, 1 } };*/ 
-cout<<"enter the number of islands";
-cin>>n;
-cout<<"enter the matrix";
-for(int i=0;i<n;i++)
-for(int j=0;j<n;j++)
-cin>>M[i][j];
-	cout << "Number of islands is: " << countIslands(M); 
-	cout<<"\n";
-	return 0; 
-} 
+int countIslands(int M[][COL])
+{
+
+    bool visited[ROW][COL];
+    memset(visited, 0, sizeof(visited));
+    int count = 0;
+    for (int i = 0; i < ROW; ++i)
+        for (int j = 0; j < COL; ++j)
+
+
+            if (M[i][j] && !visited[i][j]) {
+
+                DFS(M, i, j, visited);
+
+
+                ++count;
+            }
+
+    return count;
+}
+
+int main()
+{
+    int M[ROW][COL];
+    cout<<"enter the adjacency matrix"<<endl;
+    for(int i=0;i<ROW;i++)
+    {
+       for(int j=0;j<COL;j++)
+       {
+         cin>>M[i][j];
+       }
+    }
+    cout << "Number of islands is: " << countIslands(M);
+
+    return 0;
+}
+
